7a6d1217bd3261b087d689b76a220cdffe1e497f
Abenezer Zeleke <abenezer.zeleke>
2022-09-09 09:48:36 +0300

    Notification implemented

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { configureNotification, createChannel } from './src/utils/notification-helpers';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => {
+    configureNotification();
+    createChannel('nordstone-channel');
+    return App
+});
--- src/screens/User/NotificationScreen.jsx
+++ src/screens/User/NotificationScreen.jsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { showNotification } from '../../utils/notification-helpers'
 
 const NotificationScreen = () => {
+
+  const triggerNotification = () => {
+   showNotification()
+  }
   return (
     <View>
-      <Text>NotificationScreen</Text>
+      <Button title='Trigger Notification' onPress={triggerNotification} />
     </View>
   )
 }
